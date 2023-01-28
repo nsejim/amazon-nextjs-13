621d6e9a11acd5d877485adb6820c5827b2498c1
Jimmy NSENGA <8685602+nsejim@users.noreply.github.com>
2023-01-28 21:46:15 +0100

    Implemented header bottom

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -44,8 +44,20 @@ function Header() {
 
             </div>
             {/*** Bottom Nav*/}
-            <div>
-
+            <div className="flex items-center bg-amazon_blue-light space-x-3 text-white text-sm whitespace-nowrap p-2 pl-6">
+                <p className="flex link items-center">
+                    <MenuIcon className="h-6 w-6 mr-1" />
+                    All
+                </p>
+                <p className="link">Prime Video</p>
+                <p className="link">Amazon Business</p>
+                <p className="link">Today's Deals</p>
+                <p className="link hidden lg:inline-flex">Electronics</p>
+                <p className="link hidden lg:inline-flex">Food & Glocery</p>
+                <p className="link hidden lg:inline-flex">Prime</p>
+                <p className="link hidden lg:inline-flex">Buy again</p>
+                <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+                <p className="link hidden lg:inline-flex">Health & Personal Care</p>
             </div>
         </header>
     );
